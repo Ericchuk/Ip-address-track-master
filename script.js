a29c12166276425cd8833d3da4d8d1e1bd5eeb32
Ericchuk <47712998+Ericchuk@users.noreply.github.com>
2022-06-25 00:22:24 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+let address = document.querySelector(".ipAddress");
+let locationVal = document.querySelector(".locationVal");
+let timeZone = document.querySelector(".timezoneVal");
+let utc = document.querySelector(".utcVal");
+let isp = document.querySelector(".ispVal");
+let data = document.querySelector(".ipAddress");
+let map = document.querySelector(".mapImage");
+let input = document.querySelector("input");
+let arrow = document.querySelector("img")
+
+let url = 'https://api.ipify.org/?format=json';
+async function fetchData(){
+    let response = await fetch(url);
+    let result = await response.json();
+    input.value = result.ip;
+    // console.log(result);
+}
+fetchData();
+
+
+async function fetchDataClick(){
+    let web = "https://geo.ipify.org/api/v2/country?apiKey=at_z5c98dBARZ3etXRIc4luHcBfmabIU&ipAddress=";
+    let combineBothAPI = `${web} ${url}`
+    let dataResponse = await fetch(web);
+    let dataResult = await dataResponse.json();
+    address.innerHTML = input.value;
+    let region = await dataResult.location.region;
+    let country = await dataResult.location.country;
+    locationVal.innerHTML = `${region}, ${country}`;
+    let timeZoneValue = await dataResult.location.timezone;
+    timeZone.innerHTML = `UTC ${timeZoneValue}`
+    isp.innerHTML = await dataResult.isp
+    let lat = await dataResult.location.lat;
+    let long = await dataResult.location.lng;
+    console.log(typeof dataResult, dataResult);
+}
+fetchDataClick();
+arrow.addEventListener("click", fetchDataClick);
